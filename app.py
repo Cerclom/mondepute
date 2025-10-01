b7650bec60f8bf16e4d8e27cbf64bf4907ebd021
Cerclom <scercle@outlook.fr>
2025-10-01 17:39:53 +0200

    Ajout premier graphique vue d'ensemble

--- app.py
+++ app.py
@@ -1,20 +1,55 @@
 import streamlit as st
 import sqlite3
 import pandas as pd
+import plotly.express as px
 
 # Connexion à la base SQLite
 conn = sqlite3.connect("sql/mondepute.sqlite")
 
-st.title("MonDepute - Tableau de bord")
+st.title("Tableau de bord de l'assemblée nationale (legislature 17)")
 
-# Liste des tables disponibles
-tables = pd.read_sql("SELECT name FROM sqlite_master WHERE type='table';", conn)
-st.sidebar.write("Tables disponibles :", tables)
+# ------------------------
+# Création des onglets
+# ------------------------
+tab1, tab2, tab3 = st.tabs(["Informations générales", "Scrutins", "Députés"])
 
-# Choix de la table à afficher
-table_name = st.sidebar.selectbox("Choisir une table :", tables['name'])
+# ------------------------
+# Onglet 1 : Vue d'ensemble
+# ------------------------
 
-# Affichage de la table
-if table_name:
-    df = pd.read_sql(f"SELECT * FROM {table_name} LIMIT 1000", conn)  # limitation pour ne pas tout charger
-    st.write(df)
+with tab1:
+    st.subheader("Vue d'ensemble")
+
+    #Nombre total de députés
+    nb_deputes = pd.read_sql("SELECT COUNT(distinct uid) AS NB FROM depute", conn)["NB"][0]
+
+    #Nombre de scrutins
+    nb_scrutins = pd.read_sql("SELECT COUNT(distinct uid) AS NB FROM vote", conn)["NB"][0]
+
+    #Affichage en tableau
+    col1, col2 = st.columns(2)
+    col1.metric("Nombre de députés", nb_deputes)
+    col2.metric("Nombre de scrutins", nb_scrutins)
+
+    #Proportion de refus et d'adoption
+    #Récupération des données avec Pandas :
+    graph_pie = pd.read_sql("SELECT codesort, COUNT(*) AS NB FROM vote GROUP BY codesort", conn)
+    # Remplacer les valeurs pour avoir des majuscules
+    graph_pie["codesort"] = graph_pie["codesort"].replace({
+        "adopté": "Adopté",
+        "rejeté": "Rejeté"
+    })
+    #Affichage du graphique avec Streamlit:
+    fig = px.pie(
+        graph_pie, 
+        values="NB",
+        names="codesort",
+        title ="Proportion de scrutins adoptés et rejetés",
+        color= "codesort",
+        color_discrete_map={"Adopté":"green", "Rejeté":"red"}
+    )
+    fig.update_traces(
+        hovertemplate="%{value}"  # n'affiche que le nombre
+    )
+    #Affichage dans Streamlit
+    st.plotly_chart(fig)
